8193f97a322c0c033177017f5870ade0441a6d4f
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-05-29 14:42:41 +0000

    Update StringHelper.cs

--- VowelCountTask/StringHelper.cs
+++ VowelCountTask/StringHelper.cs
@@ -4,6 +4,14 @@ namespace VowelCountTask
 {
     public static class StringHelper
     {
+        /// <summary>
+        /// Return the count of vowels in the source string.
+        ///  a, e, i, o, and u are vowels. 
+        /// </summary>
+        /// <param name="source">Source string.</param>
+        /// <returns>Count of vowels in the given string.</returns>
+        /// <exception cref="ArgumentNullException">Thrown is string is null.</exception>
+        /// <exception cref="ArgumentException">Thrown is string is empty.</exception>
         public static int GetCountOfVowel(string source)
         {
             throw new NotImplementedException("You need to implement this function.");
